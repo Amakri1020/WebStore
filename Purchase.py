043bd84dc2360ac609784a9accf10af1e854d71c
Amakri1020 <alexander.makrigiorgos@mail.mcgill.ca>
2014-12-04 17:29:02 -0500

    working inventory update

--- Purchase.py
+++ Purchase.py
@@ -8,7 +8,7 @@ print "Content-type:text/html\n\n"
 form = cgi.FieldStorage()
 bill_total = 0
 online_users = []
-logged_in = 1
+logged_in = 0
 
 ##Load all users currently logged in into online_users##
 f = open('LoggedIn.csv')
@@ -44,18 +44,37 @@ if logged_in == 1:
     print "<title>Purchase Summary and Bill</title>"
     print "</head>"
     print "<body>"
+    print "Bill of Sales:<br>"
     for i in range(len(inventory)):
         price[i] = int(price[i])
         if (form.getvalue('checkbox'+str(i)) and form.getvalue('input_quantity'+str(i))!=None):
             q = form.getvalue('input_quantity'+str(i))
             q=int(q)
-            print "Item: %s     Quantity: %s     Price: %s<br>" %(inventory[i],q,price[i]*q)
-            bill_total += price[i]*q
-    print "Total price:  $ %s" % (bill_total)
+            if(q > quantity[i]):
+                print "Unfortunately, we only have %s of %s left in stock. We apologize for any inconvenience."%(quantity[i],inventory[i])
+            else:
+                print "Item: %s     Quantity: %s     Price: %s<br>" %(inventory[i],q,price[i]*q)
+                bill_total += price[i]*q
+    if bill_total == 0:
+        print "No items selected!"
+    else:
+        print "Total price:  $ %s" % (bill_total)
 #    print "<li class="menu-item"><a class="menu-url" href="index.html">Back to Homepage</a></li>"
 #    print "<li class="menu-item"><a class="menu-url" href="catalogue.html">Back to Catalogue</a></li>"
     print "</body>"
     print "</html>"
+    
+    data = ["Trottier,%s,7500000"%quantity[0],
+"McConnell,%s,2000000"%quantity[1],
+"MacDonald,%s,1000000"%quantity[2],
+"Leacock,%s,250000"%quantity[3],
+"McIntyre,%s,45000"%quantity[4],
+"Arts,%s,20"%quantity[5]]
+    
+    with open("Inventory.csv","wb") as csv_file:
+    writer = csv.writer(csv_file)
+    for line in data:
+        writer.writerow(line.split(","))
     
 else:
     print "Content-type:text/html\r\n\r\n"
